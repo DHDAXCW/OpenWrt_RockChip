d21e21c7aab00afaa7a265a6a2b2e24d8bc7547f
骷髅头 <lasstp5011@gmail.com>
2024-11-30 09:09:57 +0800

    Update lede.sh

--- scripts/lede.sh
+++ scripts/lede.sh
@@ -12,12 +12,6 @@
 mkdir package/community
 pushd package/community
 
-# Add Lienol's Packages
-git clone --depth=1 https://github.com/Lienol/openwrt-package
-rm -rf ../../customfeeds/luci/applications/luci-app-kodexplorer
-rm -rf openwrt-package/verysync
-rm -rf openwrt-package/luci-app-verysync
-
 # apppppppp
 git clone --depth=1 https://github.com/DHDAXCW/dhdaxcw-app
 
